1d64397183149df8b20e36a9b622b8b33fb6faa3
Elias <Elias.stoeger@gmail.com>
2020-04-28 02:26:29 +0200

    fixed a typo

--- Wasteland Fantasy (in progress)/Enemies.py
+++ Wasteland Fantasy (in progress)/Enemies.py
@@ -22,7 +22,7 @@ class Enemy:
 Enemies = [["a Ratling", "a Snider", "like 12 or 13 ants", "a Dust Golem", "a Sproutling", "3/4 of an adder"],
            ["an Adder", "an Ent", "a briddle Ghule", "25 to 30 ants", "a Fish with legs", "a bubbling pile of goo"],
            ["a Jackalope", "at least a basket of ants", "a DogCat", "a murdering mantis", "a Whispling", "a Treant"],
-           ["a Skin fairy", "hundrets of ants", "a Chimera", "a Skunk with 3 butts", "a Shark with legs", "a Reverse Centaur"],
+           ["a Skin fairy", "hundreds of ants", "a Chimera", "a Skunk with 3 butts", "a Shark with legs", "a Reverse Centaur"],
            ["a Bone Fairy", "a hulking Ant Golem", "a rabid Ursine", "a Voidcrawler", "the Left Arm of the Forbidden One", "Steve"]]
 
 # Gambler NPC -> Sphinx
